f2f8c333b53fb555f031b49d2f0f55301ca7203b
dsdlfjb <134208526+dsdlfjb@users.noreply.github.com>
2023-06-29 15:07:09 +0900

    유니티_2d 포트폴리오

---
+++ Assets/Scripts/Manager/ShopManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    [Header("[ 상점 정보 ]")]
+    public Sprite[] _swordSpriteList;
+    public string[] _swordNameList;
+    public int[] _swordSilverList;   // 해금 조건에 필요한 은동전
+    public int[] _swordCoinList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
